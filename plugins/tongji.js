6f38e1b320b6946cfedba69e998b4ab1c21e55cd
17723262771 <Nie55352618>
2022-04-18 09:51:07 +0800

    Site updated: 2022-04-18 09:51:07

--- plugins/tongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function () {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?902fdb6354ec7ec5724369a31fbf8e6c";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
